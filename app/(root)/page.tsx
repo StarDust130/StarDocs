e1194e4a8cc684e1253d41a9cf4a90c14861dd8b
ChandraShekhar <csyadav0513@gmail.com>
2024-07-22 11:59:37 +0530

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+
+export default function Home() {
+  return (
+    <main>
+
+      Hello 🔔
+      <Button>Click Me</Button>
+
+    </main>
+  );
+}
