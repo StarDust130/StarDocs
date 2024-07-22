3e4bf49d0581b7d112c9ed6d4bb67e606ddc70a2
ChandraShekhar <csyadav0513@gmail.com>
2024-07-22 13:25:38 +0530

    Auth Setup

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,8 +1,23 @@
-import { Editor } from "@/components/editor/Editor"
+import { Editor } from "@/components/editor/Editor";
+import Header from "@/components/elements/Header";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 
 const Documents = () => {
   return (
-    <div><Editor /></div>
-  )
-}
-export default Documents
+    <div>
+      <Header>
+        <div className="flex w-fit items-center justify-center gap-2 ">
+          <p className="document-title">Title</p>
+        </div>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </Header>
+      <Editor />
+    </div>
+  );
+};
+export default Documents;
